d76f37d53124d2cbe1dd7ca23f5addaae4ead9ee
IbraSuraya <hasannn.9c@gmail.com>
2024-05-21 18:52:17 +0700

    Part 2 Clear

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// Toggle class active
+// Toggle class active for hambuger menu
 const navbarNav = document.querySelector('.navbar-nav');
 
 // when hambuger menu clicked
@@ -6,14 +6,48 @@ document.querySelector('#hamburger-menu').onclick = () => {
   navbarNav.classList.toggle('active');
 };
 
-// click out sidebar for hidden sidebar
-const hamburger = document.querySelector('#hamburger-menu');
+// Toggele clase active for search form
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
+
+// when search icon clicked
+document.querySelector('#search-button').onclick = (e) => {
+  searchForm.classList.toggle('active');
+  searchBox.focus();
+  e.preventDefault();
+};
+
+// Toggle class active for support cart
+const supportCart = document.querySelector('.support-cart');
+document.querySelector('#support-cart-button').onclick = (e) => {
+  supportCart.classList.toggle('active');
+  e.preventDefault();
+};
+
+
+// click out object, hidden object
+const hmIcon = document.querySelector('#hamburger-menu');
+const searchButton = document.querySelector('#search-button');
+const supportButton = document.querySelector('#support-cart-button')
+
 document.addEventListener('click', function (e) {
-  if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+  // for hamburger icon 
+  if(!hmIcon.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove('active');
+  }
+  
+  // for search icon 
+  if(!searchButton.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove('active');
+  }
+  
+  // for support icon 
+  if(!supportButton.contains(e.target) && !supportCart.contains(e.target)) {
+    supportCart.classList.remove('active');
   }
 })
 
+// Smooth scroling page (Gagal)
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener('click', function(e) {
       e.preventDefault();
@@ -23,3 +57,27 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
       });
   });
 });
+
+// Modal Box
+const itemDetailModal = document.querySelector('#item-detail-modal')
+const itemDetailButtons = document.querySelectorAll('.item-detail-button')
+
+itemDetailButtons.forEach((btn) => {
+  btn.onclick = (e) => {
+    itemDetailModal.style.display = 'flex'
+    e.preventDefault()
+  }
+})
+
+// Close Modal Box with icon close
+document.querySelector('.modal .close-icon').onclick = (e) => {
+  itemDetailModal.style.display = 'none';
+  e.preventDefault();
+}
+
+// Close Modal Box without icon close
+window.onclick = (e) => {
+  if (e.target === itemDetailModal){
+    itemDetailModal.style.display = 'none';
+  }
+}
